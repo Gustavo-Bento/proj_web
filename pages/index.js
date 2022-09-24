cc830386160bb77029d14d271127a0f31e3958af
Gustavo Ferreira <gfmb94@gmail.com>
2022-09-24 07:59:52 -0300

    Alterando Layout

--- pages/index.js
+++ pages/index.js
@@ -2,81 +2,138 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+var NewComponent = React.createClass({
+  render: function() {
     return (
       <div>
         <title>W3.CSS Template</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway" />
-        <style dangerouslySetInnerHTML={{__html: "\nbody,h1,h5 {font-family: \"Raleway\", sans-serif}\nbody, html {height: 100%}\n.bgimg {\n  background-image: url('/onepage_restaurant.jpg');\n  min-height: 100%;\n  background-position: center;\n  background-size: cover;\n}\n" }} />
-        <div className="bgimg w3-display-container w3-text-white">
-          <div className="w3-display-middle w3-jumbo">
-            <p>logo</p>
-          </div>
-          <div className="w3-display-topleft w3-container w3-xlarge">
-            <p><button onclick="document.getElementById('menu').style.display='block'" className="w3-button w3-black">menu</button></p>
-            <p><button onclick="document.getElementById('contact').style.display='block'" className="w3-button w3-black">contact</button></p>
-          </div>
-          <div className="w3-display-bottomleft w3-container">
-            <p className="w3-xlarge">monday - friday 10-23 | saturday 14-02</p>
-            <p className="w3-large">42 village St, New York</p>
-            <p>powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-          </div>
-        </div>
-        {/* Menu Modal */}
-        <div id="menu" className="w3-modal">
-          <div className="w3-modal-content w3-animate-zoom">
-            <div className="w3-container w3-black w3-display-container">
-              <span onclick="document.getElementById('menu').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
-              <h1>Starters</h1>
-            </div>
-            <div className="w3-container">
-              <h5>Tomato Soup <b>$2.50</b></h5>
-              <h5>Chicken Salad <b>$3.50</b></h5>
-              <h5>Bread and Butter <b>$1.00</b></h5>
-            </div>
-            <div className="w3-container w3-black">
-              <h1>Main Courses</h1>
-            </div>
-            <div className="w3-container">
-              <h5>Grilled Fish and Potatoes <b>$8.50</b></h5>
-              <h5>Italian Pizza <b>$5.50</b></h5>
-              <h5>Veggie Pasta <b>$4.00</b></h5>
-              <h5>Chicken and Potatoes <b>$6.50</b></h5>
-              <h5>Deluxe Burger <b>$5.00</b></h5>
-            </div>
-            <div className="w3-container w3-black">
-              <h1>Desserts</h1>
-            </div>
-            <div className="w3-container">
-              <h5>Fruit Salad <b>$2.50</b></h5>
-              <h5>Ice cream <b>$2.00</b></h5>
-              <h5>Chocolate Cake <b>$4.00</b></h5>
-              <h5>Cheese <b>$5.50</b></h5>
-            </div>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma" />
+        <style dangerouslySetInnerHTML={{__html: "\nbody,h1,h2,h3,h4,h5,h6 {font-family: \"Karma\", sans-serif}\n.w3-bar-block .w3-bar-item {padding:20px}\n" }} />
+        {/* Sidebar (hidden by default) */}
+        <nav className="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style={{display: 'none', zIndex: 2, width: '40%', minWidth: '300px'}} id="mySidebar">
+          <a href="javascript:void(0)" onclick="w3_close()" className="w3-bar-item w3-button">Close Menu</a>
+          <a href="#food" onclick="w3_close()" className="w3-bar-item w3-button">Food</a>
+          <a href="#about" onclick="w3_close()" className="w3-bar-item w3-button">About</a>
+        </nav>
+        {/* Top menu */}
+        <div className="w3-top">
+          <div className="w3-white w3-xlarge" style={{maxWidth: '1200px', margin: 'auto'}}>
+            <div className="w3-button w3-padding-16 w3-left" onclick="w3_open()">☰</div>
+            <div className="w3-right w3-padding-16">Mail</div>
+            <div className="w3-center w3-padding-16">My Food</div>
           </div>
         </div>
-        {/* Contact Modal */}
-        <div id="contact" className="w3-modal">
-          <div className="w3-modal-content w3-animate-zoom">
-            <div className="w3-container w3-black">
-              <span onclick="document.getElementById('contact').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
-              <h1>Contact</h1>
+        {/* !PAGE CONTENT! */}
+        <div className="w3-main w3-content w3-padding" style={{maxWidth: '1200px', marginTop: '100px'}}>
+          {/* First Photo Grid*/}
+          <div className="w3-row-padding w3-padding-16 w3-center" id="food">
+            <div className="w3-quarter">
+              <img src="/w3images/sandwich.jpg" alt="Sandwich" style={{width: '100%'}} />
+              <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+              <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
             </div>
-            <div className="w3-container">
-              <p>Reserve a table, ask for today's special or just send us a message:</p>
-              <form action="/action_page.php" target="_blank">
-                <p><input className="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name" /></p>
-                <p><input className="w3-input w3-padding-16 w3-border" type="number" placeholder="How many people" required name="People" /></p>
-                <p><input className="w3-input w3-padding-16 w3-border" type="datetime-local" placeholder="Date and time" required name="date" defaultValue="2020-11-16T20:00" /></p>
-                <p><input className="w3-input w3-padding-16 w3-border" type="text" placeholder="Message \ Special requirements" required name="Message" /></p>
-                <p><button className="w3-button" type="submit">SEND MESSAGE</button></p>
-              </form>
+            <div className="w3-quarter">
+              <img src="/w3images/steak.jpg" alt="Steak" style={{width: '100%'}} />
+              <h3>Let Me Tell You About This Steak</h3>
+              <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+              <img src="/w3images/cherries.jpg" alt="Cherries" style={{width: '100%'}} />
+              <h3>Cherries, interrupted</h3>
+              <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+              <p>What else?</p>
+            </div>
+            <div className="w3-quarter">
+              <img src="/w3images/wine.jpg" alt="Pasta and Wine" style={{width: '100%'}} />
+              <h3>Once Again, Robust Wine and Vegetable Pasta</h3>
+              <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
             </div>
           </div>
+          {/* Second Photo Grid*/}
+          <div className="w3-row-padding w3-padding-16 w3-center">
+            <div className="w3-quarter">
+              <img src="/w3images/popsicle.jpg" alt="Popsicle" style={{width: '100%'}} />
+              <h3>All I Need Is a Popsicle</h3>
+              <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+              <img src="/w3images/salmon.jpg" alt="Salmon" style={{width: '100%'}} />
+              <h3>Salmon For Your Skin</h3>
+              <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+              <img src="/w3images/sandwich.jpg" alt="Sandwich" style={{width: '100%'}} />
+              <h3>The Perfect Sandwich, A Real Classic</h3>
+              <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+              <img src="/w3images/croissant.jpg" alt="Croissant" style={{width: '100%'}} />
+              <h3>Le French</h3>
+              <p>Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+          </div>
+          {/* Pagination */}
+          <div className="w3-center w3-padding-32">
+            <div className="w3-bar">
+              <a href="#" className="w3-bar-item w3-button w3-hover-black">«</a>
+              <a href="#" className="w3-bar-item w3-black w3-button">1</a>
+              <a href="#" className="w3-bar-item w3-button w3-hover-black">2</a>
+              <a href="#" className="w3-bar-item w3-button w3-hover-black">3</a>
+              <a href="#" className="w3-bar-item w3-button w3-hover-black">4</a>
+              <a href="#" className="w3-bar-item w3-button w3-hover-black">»</a>
+            </div>
+          </div>
+          <hr id="about" />
+          {/* About Section */}
+          <div className="w3-container w3-padding-32 w3-center">  
+            <h3>About Me, The Food Man</h3><br />
+            <img src="/w3images/chef.jpg" alt="Me" className="w3-image" style={{display: 'block', margin: 'auto'}} width={800} height={533} />
+            <div className="w3-padding-32">
+              <h4><b>I am Who I Am!</b></h4>
+              <h6><i>With Passion For Real, Good Food</i></h6>
+              <p>Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an interest of lorem ipsum and mauris neque quam blog. I want to share my world with you. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+            </div>
+          </div>
+          <hr />
+          {/* Footer */}
+          <footer className="w3-row-padding w3-padding-32">
+            <div className="w3-third">
+              <h3>FOOTER</h3>
+              <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+              <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+            </div>
+            <div className="w3-third">
+              <h3>BLOG POSTS</h3>
+              <ul className="w3-ul w3-hoverable">
+                <li className="w3-padding-16">
+                  <img src="/w3images/workshop.jpg" className="w3-left w3-margin-right" style={{width: '50px'}} />
+                  <span className="w3-large">Lorem</span><br />
+                  <span>Sed mattis nunc</span>
+                </li>
+                <li className="w3-padding-16">
+                  <img src="/gondol.jpg" className="w3-left w3-margin-right" style={{width: '50px'}} />
+                  <span className="w3-large">Ipsum</span><br />
+                  <span>Praes tinci sed</span>
+                </li> 
+              </ul>
+            </div>
+            <div className="w3-third w3-serif">
+              <h3>POPULAR TAGS</h3>
+              <p>
+                <span className="w3-tag w3-black w3-margin-bottom">Travel</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">New York</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dinner</span>
+                <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Salmon</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">France</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Drinks</span>
+                <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Ideas</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Flavors</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Cuisine</span>
+                <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Chicken</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dressing</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Fried</span>
+                <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Fish</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Duck</span>
+              </p>
+            </div>
+          </footer>
+          {/* End page content */}
         </div>
       </div>
     );
   }
+});
